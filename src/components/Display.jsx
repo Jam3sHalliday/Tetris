ee1755b7334b83dab3a6c3e3d6977004bdb248c8
Jam3sHalliday <quanggtangg@gmail.com>
2020-04-11 21:09:26 +0700

    first commit tetris game but incompleted

---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyledDisplay } from './styled/StyledDisplay';
+
+const Display = ({gameOver,  text}) => (
+    <StyledDisplay gameOver = { gameOver }>
+        { text }
+    </StyledDisplay>
+)
+
+export default Display;
